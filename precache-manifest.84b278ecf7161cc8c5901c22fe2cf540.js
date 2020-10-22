a0d62dd3dbee71a887e1eefc0ab42c36e8762c50
Siddhartha Rusiya <siddhatharusiya84@gmail.com>
2020-10-22 21:11:54 +0530

    Updates

---
+++ precache-manifest.84b278ecf7161cc8c5901c22fe2cf540.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7af9a7c013ad5ed22097c048e327e9a2",
+    "url": "/Income-Tracker/index.html"
+  },
+  {
+    "revision": "b17a9dde19b6931c4939",
+    "url": "/Income-Tracker/static/css/main.d0e32a8e.chunk.css"
+  },
+  {
+    "revision": "570dd6c2e2d96ccd72cb",
+    "url": "/Income-Tracker/static/js/2.fde521a5.chunk.js"
+  },
+  {
+    "revision": "65075b20798fa24012a1ad453c78c90d",
+    "url": "/Income-Tracker/static/js/2.fde521a5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b17a9dde19b6931c4939",
+    "url": "/Income-Tracker/static/js/main.f1892cc6.chunk.js"
+  },
+  {
+    "revision": "9a4d04f14f81159114c5",
+    "url": "/Income-Tracker/static/js/runtime-main.12558b40.js"
+  }
+]);
